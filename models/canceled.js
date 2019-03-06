1e750b1376acec333754ce9debea0e83e1701917
Camille <c.hughes64@gmail.com>
2019-03-05 20:57:29 -0500

    sequelize models

---
+++ models/canceled.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes){
+    var Canceled= sequelize.define("Canceled", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            max: 301
+        }
+    },
+    {
+    freezeTableName: true
+    });
+
+    Canceled.associate = function(models){
+        Canceled.belongsTo(models.Reservation_Room,{
+            foreignKey :{
+                allowNull: false
+            }
+        });
+    }
+    return Canceled;
+};
