66b1c7a2ee11532aabb2f0cd526924cacdb5a7c8
Camille <c.hughes64@gmail.com>
2019-03-09 13:37:59 -0500

    ajax begin

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
 //routing in static app.use in server.js
 //this is where we do our jquery onclicks and also the ajax put/post to push the info into our api arrays
+
+$(document).ready(function () {
+//GUEST
+    //SEARCH ALL
+    $("#searchAllGuests").on("click", function(event) {
+        $.ajax("/api/guests/all", {
+            type: "GET"
+        }).then(function () {
+            console.log("All guests are displayed. Use form to narrow your search.");
+            
+            //this reloads page, must change
+            location.reload(); //we don't want the whole page to reload, just the list to update as we go
+        })
+    });
+    
+
+}); //END DOC.READY
