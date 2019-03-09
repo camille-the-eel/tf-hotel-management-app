74ea5830101cbceed00d848ef0e6ca8cb8b90ad7
arieru <arieru@gmail.com>
2019-03-09 13:35:45 -0500

    more routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,37 +2,42 @@ var db = require("../models");
 var moment = require('moment');
 
 
-module.exports = function(app){
-    app.get("/", function(req, res){
-        db.Reservation.findAll().then(function(dbReservation){
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        db.Reservation.findAll().then(function (dbReservation) {
             // res.render("index", {reservation : dbReservation});
             console.log("reservation data: ", dbReservation);
-            db.Guest.findAll({
-                where : {
-                    
-                }
-            }).then(function(dbGuest){
+            db.Guest.findAll({}).then(function (dbGuest) {
                 // console.log("guest data: ", dbGuest);
-                res.render("index", {
-                    guest : dbGuest,
-                    reservation : dbReservation
+                db.Rooms.findAll({}).then(function (dbRooms) {
+                    // console.log("rooms data: ", dbRooms);
+                    db.Reservation_Room.findAll({
+                        where: {
+                            in_house : 1
+                        }
+                    }).then(function (dbReservationRoom) {
+                        console.log("reservation room data: ", dbReservationRoom);
+
+                        res.render("index", {
+                            guest: dbGuest,
+                            reservation: dbReservation,
+                            room : dbRooms,
+                            reservation_room: dbReservationRoom
+                        });
+                    });
                 });
-                
+
             });
         });
-        
-        
-        
+
     });
-    // app.get("/", function(req, res){
-       
-        
-    // });
 
 
-    app.get("/reservation/new", function(req, res){
-        db.Rooms.findAll({}).then(function(dbRooms){
-            res.render("new-reservation", {rooms : dbRooms});
+
+
+    app.get("/reservation/new", function (req, res) {
+        db.Rooms.findAll({}).then(function (dbRooms) {
+            res.render("new-reservation", { rooms: dbRooms });
         });
     });
 };
