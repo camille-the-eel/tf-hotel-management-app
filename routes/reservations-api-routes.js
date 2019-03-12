76ecbfe1de557353a27c5cf3181e049eebdef949
arieru <arieru@gmail.com>
2019-03-12 15:46:27 -0400

    not done yet

--- routes/reservations-api-routes.js
+++ routes/reservations-api-routes.js
@@ -1,9 +1,35 @@
-var db = require("..models/");
+var db = require("../models");
 
 module.exports = function(app){
 
-    app.get("/api/reservations", function(req, res){
-        var query = {};
-        if (req.query.guest)
+    app.get("/api/reservations/:condition", function(req, res){
+        db.Reservation.findOne({
+            include : [{
+                model: db.Guest,
+                where : {
+                   first_name: req.params.condition
+                }
+            }],
+            where : {
+                id: req.params.condition
+            }
+        }).then(function(dbReservation){
+            res.json(dbReservation);
+        })
     });
+
+    // app.get("/api/reservations/Canceled/:condition", function(req, res){
+    //     db.Reservation_Room.findOne({
+    //         include: [{
+    //             model: db.Reservation,
+    //             include: [db.Guest]
+    //         }],
+    //         where: {
+    //             canceled: req.params.condition
+    //         }
+    //     }).then(function(dbReservation){
+    //         res.json(dbReservation);
+    //     });
+    // });
+        
 };
