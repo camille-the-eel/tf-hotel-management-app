ab6cbc0195a9f94ea648fc59d5621998e3c3b1bb
Camille <c.hughes64@gmail.com>
2019-03-14 15:31:22 -0400

    partials include edit

--- public/js/app.js
+++ public/js/app.js
@@ -334,7 +334,7 @@ $(document).ready(function () {
             }
         $.ajax({
             // url : "/reservation/new/roomsearch"
-            url : "/reservation/new?start_date=" + condition.startDate + "&end_date=" + condition.endDate
+            url : "/reservation/new/roomsearch?start_date=" + condition.startDate + "&end_date=" + condition.endDate
             // url : "/reservation/new/roomsearch?start_date=" + condition.startDate + "&end_date=" + condition.endDate
         }).then(function(data){
             resroomscontainer.html(data);
