e4157a7591cd3bf566571bb9f28381fac5db7ee5
Camille <c.hughes64@gmail.com>
2019-03-14 19:30:41 -0400

    ~

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(express.static("public"));
-app.set("view engine", "EJS");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 
