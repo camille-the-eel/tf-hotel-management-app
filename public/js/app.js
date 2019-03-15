b318e53f7d48588a164b094d0698a35a8417f7b5
Camille <c.hughes64@gmail.com>
2019-03-14 23:34:48 -0400

    index formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-//routing in static app.use in server.js
-//this is where we do our jquery onclicks and also the ajax put/post to push the info into our api arrays
-
 $(document).ready(function () {
 
 //===================================================================
